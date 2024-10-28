c5842787c8da0a00626d3e5346ef7df3ee58a02f
kid <113590185+afterlife44@users.noreply.github.com>
2024-10-28 20:50:56 +0300

    Update tictactoe.cpp

--- tictactoe.cpp
+++ tictactoe.cpp
@@ -1,176 +1,267 @@
 #include <iostream>
 #include <vector>
+#include <limits>
 
 using namespace std;
 
-// Класс, представляющий игру в крестики-нолики
-class TicTacToe {
-private:
-    int size;             // Размер игрового поля
-    int winLength;        // Длина выигрышной последовательности
-    vector<vector<char>> board;  // Игровое поле в виде двумерного вектора символов
-
-public:
-    // Конструктор для инициализации игрового поля с указанным размером и длиной выигрышной последовательности
-    TicTacToe(int _size, int _winLength) : size(_size), winLength(_winLength) {
-        // Инициализация игрового поля пустыми ячейками
-        board = vector<vector<char>>(size, vector<char>(size, ' '));
-    }
-
-    // Отображение текущего состояния игрового поля
-    void displayBoard() const {
-        for (int i = 0; i < size; ++i) {
-            for (int j = 0; j < size; ++j) {
-                cout << board[i][j] << " ";
-            }
-            cout << endl;
-        }
-        cout << endl;
-    }
-
-    // Выполнение хода игрока
-    bool makeMove(int row, int col, char symbol) {
-        // Адаптация координат от 1 до size
-        --row;
-        --col;
-
-        // Проверка корректности хода и установка символа в ячейку
-        if (row < 0 || row >= size || col < 0 || col >= size || board[row][col] != ' ') {
-            return false;
-        }
-
-        board[row][col] = symbol;
-        return true;
-    }
-
-    // Проверка, является ли текущий символ победителем
-    bool isWinner(char symbol) const {
-        // Проверка по строкам и столбцам
-        for (int i = 0; i < size; ++i) {
-            int countRow = 0;
-            int countCol = 0;
-            for (int j = 0; j < size; ++j) {
-                if (board[i][j] == symbol) {
-                    ++countRow;
-                }
-                if (board[j][i] == symbol) {
-                    ++countCol;
-                }
-            }
-            if (countRow == winLength || countCol == winLength) {
-                return true;
-            }
-        }
-
-        // Проверка по диагоналям
-        for (int i = 0; i <= size - winLength; ++i) {
-            for (int j = 0; j <= size - winLength; ++j) {
-                int countDiagonal1 = 0;
-                int countDiagonal2 = 0;
-                for (int k = 0; k < winLength; ++k) {
-                    if (board[i + k][j + k] == symbol) {
-                        ++countDiagonal1;
-                    }
-                    if (board[i + k][j + winLength - 1 - k] == symbol) {
-                        ++countDiagonal2;
-                    }
-                }
-                if (countDiagonal1 == winLength || countDiagonal2 == winLength) {
-                    return true;
-                }
-            }
-        }
-
-        return false;
-    }
-
-    // Проверка, заполнено ли игровое поле
-    bool isBoardFull() const {
-        for (int i = 0; i < size; ++i) {
-            for (int j = 0; j < size; ++j) {
-                if (board[i][j] == ' ') {
-                    return false;
-                }
-            }
-        }
-        return true;
-    }
-};
-
-// Абстрактный класс, представляющий игрока
 class Player {
-protected:
-    char symbol;  // Символ игрока (X или O)
-
 public:
-    // Конструктор для установки символа игрока
-    Player(char _symbol) : symbol(_symbol) {}
-
-    // Виртуальная функция для выполнения хода игрока
-    virtual void makeMove(TicTacToe& game) = 0;
+    virtual void makeMove(vector<vector<char>>& board, char symbol) = 0;
 };
 
-// Класс, представляющий человеческого игрока
 class HumanPlayer : public Player {
 public:
-    // Конструктор для установки символа человеческого игрока
-    HumanPlayer(char _symbol) : Player(_symbol) {}
-
-    // Реализация функции для выполнения хода человеческого игрока
-    void makeMove(TicTacToe& game) override {
-        int row, col;
-        cout << "Enter your move (row and column): ";
-        cin >> row >> col;
-
-        // Повторный ввод, пока ход некорректен
-        while (!game.makeMove(row, col, symbol)) {
-            cout << "Incorrect move. Try again: ";
-            cin >> row >> col;
+    void makeMove(vector<vector<char>>& board, char symbol) override {
+        int x, y;
+        while (true) {
+            cout << "Enter your move (row and column starting from 1): ";
+            cin >> x >> y;
+            x--; y--;
+            if (x >= 0 && x < board.size() && y >= 0 && y < board.size() && board[x][y] == ' ') {
+                board[x][y] = symbol;
+                break;
+            } else {
+                cout << "Invalid move. Try again.\n";
+            }
         }
     }
 };
 
-// Основная функция программы
-int main() {
-    const int boardSize = 3;   // Размер игрового поля
-    const int winLength = 3;   // Длина выигрышной последовательности
+class Bot : public Player {
+public:
+    void makeMove(vector<vector<char>>& board, char symbol) override {
+        int bestScore = numeric_limits<int>::min();
+        int bestRow = -1, bestCol = -1;
 
-    // Создание объекта игры и двух человеческих игроков
-    TicTacToe game(boardSize, winLength);
-    HumanPlayer playerX('X');
-    HumanPlayer playerO('O');
-
-    // Игровой цикл до завершения игры (победа, ничья)
-    while (!game.isBoardFull() && !game.isWinner('X') && !game.isWinner('O')) {
-        // Ход игрока X
-        playerX.makeMove(game);
-        game.displayBoard();
-
-        // Проверка победы или ничьей после хода игрока X
-        if (game.isWinner('X')) {
-            cout << "Player X wins!" << endl;
-            break;
+        for (int i = 0; i < board.size(); i++) {
+            for (int j = 0; j < board.size(); j++) {
+                if (board[i][j] == ' ') {
+                    board[i][j] = symbol;
+                    int score = minimax(board, false, symbol, getOpponent(symbol));
+                    board[i][j] = ' ';
+                    if (score > bestScore) {
+                        bestScore = score;
+                        bestRow = i;
+                        bestCol = j;
+                    }
+                }
+            }
         }
-        else if (game.isBoardFull()) {
-            cout << "Draw!" << endl;
-            break;
+        board[bestRow][bestCol] = symbol;
+        cout << "Bot made a move at (" << bestRow + 1 << ", " << bestCol + 1 << ")\n";
+    }
+
+private:
+    int minimax(vector<vector<char>>& board, bool isMaximizing, char botSymbol, char playerSymbol) {
+        if (checkWin(board, botSymbol)) return 1;
+        if (checkWin(board, playerSymbol)) return -1;
+        if (isBoardFull(board)) return 0;
+
+        int bestScore = isMaximizing ? numeric_limits<int>::min() : numeric_limits<int>::max();
+
+        for (int i = 0; i < board.size(); i++) {
+            for (int j = 0; j < board.size(); j++) {
+                if (board[i][j] == ' ') {
+                    board[i][j] = isMaximizing ? botSymbol : playerSymbol;
+                    int score = minimax(board, !isMaximizing, botSymbol, playerSymbol);
+                    board[i][j] = ' ';
+                    bestScore = isMaximizing ? max(score, bestScore) : min(score, bestScore);
+                }
+            }
+        }
+        return bestScore;
+    }
+
+    bool checkWin(const vector<vector<char>>& board, char symbol) {
+        int size = board.size();
+
+        // Проверка строк
+        for (int i = 0; i < size; i++) {
+            bool rowWin = true;
+            for (int j = 0; j < size; j++) {
+                if (board[i][j] != symbol) {
+                    rowWin = false;
+                    break;
+                }
+            }
+            if (rowWin) return true;
         }
 
-        // Ход игрока O
-        playerO.makeMove(game);
-        game.displayBoard();
-
-        // Проверка победы или ничьей после хода игрока O
-        if (game.isWinner('O')) {
-            cout << "Player O wins!" << endl;
-            break;
+        // Проверка столбцов
+        for (int i = 0; i < size; i++) {
+            bool colWin = true;
+            for (int j = 0; j < size; j++) {
+                if (board[j][i] != symbol) {
+                    colWin = false;
+                    break;
+                }
+            }
+            if (colWin) return true;
         }
-        else if (game.isBoardFull()) {
-            cout << "Draw!" << endl;
-            break;
+
+        // Проверка главной диагонали
+        bool mainDiagonalWin = true;
+        for (int i = 0; i < size; i++) {
+            if (board[i][i] != symbol) {
+                mainDiagonalWin = false;
+                break;
+            }
+        }
+        if (mainDiagonalWin) return true;
+
+        // Проверка побочной диагонали
+        bool secondaryDiagonalWin = true;
+        for (int i = 0; i < size; i++) {
+            if (board[i][size - i - 1] != symbol) {
+                secondaryDiagonalWin = false;
+                break;
+            }
+        }
+        return secondaryDiagonalWin;
+    }
+
+    bool isBoardFull(const vector<vector<char>>& board) {
+        for (const auto& row : board)
+            for (char cell : row)
+                if (cell == ' ') return false;
+        return true;
+    }
+
+    char getOpponent(char symbol) {
+        return (symbol == 'X') ? 'O' : 'X';
+    }
+};
+
+class TicTacToe {
+private:
+    vector<vector<char>> board;
+    HumanPlayer player;
+    Bot bot;
+    const char PLAYER_SYMBOL = 'X';
+    const char BOT_SYMBOL = 'O';
+    int size;
+
+public:
+    TicTacToe(int boardSize) : size(boardSize), board(boardSize, vector<char>(boardSize, ' ')) {}
+
+    void displayBoard() {
+        cout << "  ";
+        for (int i = 1; i <= size; ++i) cout << i << " ";
+        cout << "\n";
+
+        for (int i = 0; i < size; i++) {
+            cout << i + 1 << " ";
+            for (int j = 0; j < size; j++) {
+                cout << board[i][j];
+                if (j < size - 1) cout << "|";
+            }
+            cout << "\n";
+            if (i < size - 1) {
+                cout << "  ";
+                for (int k = 0; k < size; ++k) cout << "--";
+                cout << "-\n";
+            }
         }
     }
 
+    bool isBoardFull() {
+        for (const auto &row : board)
+            for (char cell : row)
+                if (cell == ' ') return false;
+        return true;
+    }
+
+    bool checkWin(char symbol) {
+        int size = board.size();
+
+        // Проверка строк
+        for (int i = 0; i < size; i++) {
+            bool rowWin = true;
+            for (int j = 0; j < size; j++) {
+                if (board[i][j] != symbol) {
+                    rowWin = false;
+                    break;
+                }
+            }
+            if (rowWin) return true;
+        }
+
+        // Проверка столбцов
+        for (int i = 0; i < size; i++) {
+            bool colWin = true;
+            for (int j = 0; j < size; j++) {
+                if (board[j][i] != symbol) {
+                    colWin = false;
+                    break;
+                }
+            }
+            if (colWin) return true;
+        }
+
+        // Проверка главной диагонали
+        bool mainDiagonalWin = true;
+        for (int i = 0; i < size; i++) {
+            if (board[i][i] != symbol) {
+                mainDiagonalWin = false;
+                break;
+            }
+        }
+        if (mainDiagonalWin) return true;
+
+        // Проверка побочной диагонали
+        bool secondaryDiagonalWin = true;
+        for (int i = 0; i < size; i++) {
+            if (board[i][size - i - 1] != symbol) {
+                secondaryDiagonalWin = false;
+                break;
+            }
+        }
+        return secondaryDiagonalWin;
+    }
+
+    void playGame() {
+        while (true) {
+            displayBoard();
+            cout << "Your turn:\n";
+            player.makeMove(board, PLAYER_SYMBOL);
+            if (checkWin(PLAYER_SYMBOL)) {
+                displayBoard();
+                cout << "Congratulations! You win!\n";
+                break;
+            }
+            if (isBoardFull()) {
+                displayBoard();
+                cout << "It's a draw!\n";
+                break;
+            }
+
+            cout << "Bot's turn:\n";
+            bot.makeMove(board, BOT_SYMBOL);
+            if (checkWin(BOT_SYMBOL)) {
+                displayBoard();
+                cout << "Bot wins! Better luck next time!\n";
+                break;
+            }
+            if (isBoardFull()) {
+                displayBoard();
+                cout << "It's a draw!\n";
+                break;
+            }
+        }
+    }
+};
+
+int main() {
+    int boardSize;
+    cout << "Enter the size of the board: ";
+    cin >> boardSize;
+
+    if (boardSize < 3) {
+        cout << "The board size must be at least 3.\n";
+        return 1;
+    }
+
+    TicTacToe game(boardSize);
+    game.playGame();
     return 0;
 }
-
